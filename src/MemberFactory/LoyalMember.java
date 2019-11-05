a4d6d5d271e92c9873e04f857ebc7135aa9349bd
hogan <hogan.sean1999@gmail.com>
2019-11-05 21:13:44 +0000

    Loyal class added

--- src/MemberFactory/LoyalMember.java
+++ src/MemberFactory/LoyalMember.java
@@ -1,4 +1,18 @@
 package MemberFactory;
 
 public class LoyalMember extends Member {
+
+    public LoyalMember() {
+
+        UI_forFactory UI = new UI_forFactory();
+
+        memberType = "";
+        name = UI.nameText.getText();
+        ;
+        age = "";
+        address = "";
+
+        //address = "9 Hunters Wood, Ballyseedy, Tralee, Co. Kerry";
+        duration = "";
+    }
 }
