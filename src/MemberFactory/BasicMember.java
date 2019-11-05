f61d25d9fcebd3385dac1f39c77c943717d1ac93
hogan <hogan.sean1999@gmail.com>
2019-11-05 21:13:22 +0000

    basicMember class added

--- src/MemberFactory/BasicMember.java
+++ src/MemberFactory/BasicMember.java
@@ -2,5 +2,17 @@ package MemberFactory;
 
 public class BasicMember extends Member {
 
+    public BasicMember() {
 
+        UI_forFactory UI = new UI_forFactory();
+
+        memberType = "";
+        name = UI.nameText.getText();
+        ;
+        age = "";
+        address = "";
+
+        //address = "9 Hunters Wood, Ballyseedy, Tralee, Co. Kerry";
+        duration = "";
+    }
 }
