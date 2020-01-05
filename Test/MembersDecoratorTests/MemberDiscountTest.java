d5e53d3ab3f3fc734c9f4426b132340ec52d8829
Seán Hogan <hogan.sean1999@gmail.com>
2020-01-05 18:17:28 +0000

    Final Commit

--- Test/MembersDecoratorTests/MemberDiscountTest.java
+++ Test/MembersDecoratorTests/MemberDiscountTest.java
@@ -1,45 +1,45 @@
-//package MembersDecoratorTests;
-//
-//import MembersDecorator.*;
-//import org.junit.Test;
-//
-//import static junit.framework.Assert.assertFalse;
-//import static junit.framework.TestCase.*;
-//
-//public class MemberDiscountTest {
-//
-//    @Test
-//    public void whenGettingDiscount_makeSureItIsCorrectRate()
-//    {
-//        MemberDiscount studentWithHalfPrice = new StudentDiscount();
-//        studentWithHalfPrice = new HalfPrice(studentWithHalfPrice);
-//
-//        double expectedResult = 0.6;
-//        double actualResult   = studentWithHalfPrice.discountRate();
-//
-//        assertEquals(expectedResult, actualResult);
-//    }
-//
-//    @Test
-//    public void checkIfTestFailsWhenDiscountRateIsIncorrect()
-//    {
-//        MemberDiscount studentWithHalfPrice = new StudentDiscount();
-//        studentWithHalfPrice = new HalfPrice(studentWithHalfPrice);
-//
-//        double expectedResult = 0.61;
-//        double actualResult   = studentWithHalfPrice.discountRate();
-//
-//        assertNotSame(expectedResult, actualResult);
-//    }
-//
-//    @Test
-//    public void checkIfLoyalDiscountRateIsCorrect()
-//    {
-//        MemberDiscount LoyalDiscount = new LoyalDiscount();
-//
-//        double expectedResult = 0.15;
-//        double actualResult   = LoyalDiscount.discountRate();
-//
-//        assertEquals(expectedResult, actualResult);
-//    }
-//}
+package MembersDecoratorTests;
+
+import MembersDecorator.*;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.*;
+
+public class MemberDiscountTest {
+
+    @Test
+    public void whenGettingDiscount_makeSureItIsCorrectRate()
+    {
+        MemberDiscount studentWithHalfPrice = new StudentDiscount();
+        studentWithHalfPrice = new HalfPrice(studentWithHalfPrice);
+
+        double expectedResult = 0.6;
+        double actualResult   = studentWithHalfPrice.discountRate();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkIfTestFailsWhenDiscountRateIsIncorrect()
+    {
+        MemberDiscount studentWithHalfPrice = new StudentDiscount();
+        studentWithHalfPrice = new HalfPrice(studentWithHalfPrice);
+
+        double expectedResult = 0.61;
+        double actualResult   = studentWithHalfPrice.discountRate();
+
+        assertNotSame(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkIfLoyalDiscountRateIsCorrect()
+    {
+        MemberDiscount LoyalDiscount = new LoyalDiscount();
+
+        double expectedResult = 0.15;
+        double actualResult   = LoyalDiscount.discountRate();
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
